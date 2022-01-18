4bcc5affcaa4318150887652b8c4cebd5138e6f2
EC2 Default User <ec2-user@ip-172-31-27-62.ap-northeast-1.compute.internal>
2022-01-18 14:11:22 +0000

    電卓課題提出

--- main2.js
+++ main2.js
@@ -10,14 +10,22 @@
 let result = document.getElementById("result");
 let point = document.getElementById("point");
 let number = document.getElementsByClassName("number").value;
+let pointFlag = false;
 //ACボタン
 function reset() {
     result.value = 0;
+    if(point.disabled ==　true) {
+        point.disabled = false;
+    }
+    
 };
 
 //計算結果を表示
 function equal() {
     result.value = eval(result.value);
+    if(point.disabled ==　true) {
+        point.disabled = false;
+    }
 }
 
 //数値を入力
@@ -40,6 +48,9 @@ function operate(btn) {
     } else {
         result.value += btn.value;
     }
+    if(point.disabled ==　true) {
+        point.disabled = false;
+    }
 }
 
 //小数点が２回押せてしまう
@@ -47,11 +58,11 @@ function operate(btn) {
 //小数点を一度入力したら、２回目以降は処理をストップする
 
 //小数点の制御
-let pointFlag = false;
+
 function clickPoint(btn) {
     pointFlag = true
     if (pointFlag == true) {
-        result.innerHTML = "";
+        point.disabled = true;
     }
     if (result.value === "0" + ".") {
         result.innerHTML = "0.";
